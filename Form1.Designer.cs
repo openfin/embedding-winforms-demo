694d480568d8bd9f367e72d0047b0bf34332191c
Ricardo de pena <ricardo.depena@gmail.com>
2015-05-05 15:43:24 -0400

    added interapplication bus integration

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,42 +40,72 @@ namespace EmbeddingWindowsExample
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Node2");
-            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
-            treeNode13});
-            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Node5");
-            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
-            treeNode15});
-            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Node6");
-            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Node7");
-            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Node4", new System.Windows.Forms.TreeNode[] {
-            treeNode17,
-            treeNode18});
-            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Node10");
-            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Node8", new System.Windows.Forms.TreeNode[] {
-            treeNode20});
-            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Node11");
-            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Node9", new System.Windows.Forms.TreeNode[] {
-            treeNode22});
-            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("C# App Content", new System.Windows.Forms.TreeNode[] {
-            treeNode14,
-            treeNode16,
-            treeNode19,
-            treeNode21,
-            treeNode23});
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblConnectionStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lblConnectionStatus = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
-            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "OpenFin Connection:";
+            // 
+            // lblConnectionStatus
+            // 
+            this.lblConnectionStatus.AutoSize = true;
+            this.lblConnectionStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnectionStatus.Location = new System.Drawing.Point(174, 8);
+            this.lblConnectionStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblConnectionStatus.Name = "lblConnectionStatus";
+            this.lblConnectionStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblConnectionStatus.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Selected Data:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 31);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(231, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Click on the chart to select data";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 75);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 10;
             // 
             // tabControl1
             // 
@@ -84,23 +114,20 @@ namespace EmbeddingWindowsExample
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(10, 25);
+            this.tabControl1.Location = new System.Drawing.Point(13, 100);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1321, 666);
-            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
-            this.tabControl1.TabIndex = 5;
+            this.tabControl1.Size = new System.Drawing.Size(1310, 591);
+            this.tabControl1.TabIndex = 11;
             // 
             // tabPage1
             // 
-            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1313, 640);
+            this.tabPage1.Size = new System.Drawing.Size(1302, 565);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "OpenFin App 1";
+            this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -108,134 +135,27 @@ namespace EmbeddingWindowsExample
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(734, 458);
+            this.tabPage2.Size = new System.Drawing.Size(1302, 550);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "OpenFin App 2";
+            this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Controls.Add(this.label1);
-            this.tabPage3.Controls.Add(this.comboBox1);
-            this.tabPage3.Controls.Add(this.checkedListBox1);
-            this.tabPage3.Controls.Add(this.treeView1);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(734, 458);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "C# Tab";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(262, 143);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "This is a C# Tab.";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Suspendisse lectus leo",
-            "Consectetur in tempor sit amet",
-            "Placerat quis neque",
-            "Etiam luctus porttitor lorem ",
-            "Sed suscipit est rutrum non",
-            "Curabitur lobortis nisl a enim congue semper",
-            "Aenean commodo ultrices imperdiet",
-            "Vestibulum ut justo vel sapien"});
-            this.comboBox1.Location = new System.Drawing.Point(261, 115);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(232, 21);
-            this.comboBox1.TabIndex = 2;
-            // 
-            // checkedListBox1
-            // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
-            "Vivamus fermentum semper porta",
-            "Nunc diam velit",
-            "Adipiscing ut tristique vitae",
-            "Aagittis vel odio",
-            "Maecenas convallis ullamcorper ultricies",
-            "Curabitur ornare"});
-            this.checkedListBox1.Location = new System.Drawing.Point(261, 15);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(232, 94);
-            this.checkedListBox1.TabIndex = 1;
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(6, 15);
-            this.treeView1.Name = "treeView1";
-            treeNode13.Name = "Node2";
-            treeNode13.Text = "Node2";
-            treeNode14.Name = "Node1";
-            treeNode14.Text = "Node1";
-            treeNode15.Name = "Node5";
-            treeNode15.Text = "Node5";
-            treeNode16.Name = "Node3";
-            treeNode16.Text = "Node3";
-            treeNode17.Name = "Node6";
-            treeNode17.Text = "Node6";
-            treeNode18.Name = "Node7";
-            treeNode18.Text = "Node7";
-            treeNode19.Name = "Node4";
-            treeNode19.Text = "Node4";
-            treeNode20.Name = "Node10";
-            treeNode20.Text = "Node10";
-            treeNode21.Name = "Node8";
-            treeNode21.Text = "Node8";
-            treeNode22.Name = "Node11";
-            treeNode22.Text = "Node11";
-            treeNode23.Name = "Node9";
-            treeNode23.Text = "Node9";
-            treeNode24.Checked = true;
-            treeNode24.Name = "Node0";
-            treeNode24.Text = "C# App Content";
-            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode24});
-            this.treeView1.Size = new System.Drawing.Size(249, 437);
-            this.treeView1.TabIndex = 0;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 9);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "OpenFin Connection:";
-            // 
-            // lblConnectionStatus
-            // 
-            this.lblConnectionStatus.AutoSize = true;
-            this.lblConnectionStatus.Location = new System.Drawing.Point(122, 9);
-            this.lblConnectionStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblConnectionStatus.Name = "lblConnectionStatus";
-            this.lblConnectionStatus.Size = new System.Drawing.Size(0, 13);
-            this.lblConnectionStatus.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1335, 703);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.lblConnectionStatus);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.tabControl1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MinimumSize = new System.Drawing.Size(560, 560);
             this.Name = "Form1";
             this.Text = "Embeded OpenFin Applications";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage3.ResumeLayout(false);
-            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,16 +163,14 @@ namespace EmbeddingWindowsExample
 
         #endregion
 
+        private Label label2;
+        private Label lblConnectionStatus;
+        private Label label3;
+        private Label label4;
+        private Label label5;
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
-        private Label label2;
-        private Label lblConnectionStatus;
-        private TabPage tabPage3;
-        private TreeView treeView1;
-        private CheckedListBox checkedListBox1;
-        private Label label1;
-        private ComboBox comboBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows.Forms;
 using Openfin.WinForm;
 using Openfin.Desktop;
+using Newtonsoft.Json.Linq;
 
 namespace EmbeddingWindowsExample
 {
@@ -34,7 +35,7 @@ namespace EmbeddingWindowsExample
             //We want to re-use the chart application and create a new window for it, lets wait until its ready.
             chartEmbeddedView.OnReady += (sender, e) =>
             {
-                var windowOptions = new WindowOptions("docs-page", "http://cdn.openfin.co/jsdocs/3.0.1.5/");
+                var windowOptions = new WindowOptions("jsdocs", "http://cdn.openfin.co/jsdocs/3.0.1.5/");
                 //by passing the runtimeOptions, an existing application and windowOptions we will create a new window for that application that will be embedded in our view.
                 docsEmbeddedView.Initialize(runtimeOptions, chartEmbeddedView.OpenfinApplication, windowOptions);
             };
@@ -42,12 +43,22 @@ namespace EmbeddingWindowsExample
 
             //We can get the instance of the singleton runtime object by usig the GetRuntimeInstance function and passing 
             var openFinRuntime = Runtime.GetRuntimeInstance(runtimeOptions);
-
+            
             //THIS IS OPTIONAL, Each Embedded view will connect directly or use an existing connection, the connect function accepts an action that will either be called when the runtime connects or be called right away if the runtime is already connected.
-            openFinRuntime.Connect(() => Utils.InvokeOnUiThreadIfRequired(this, () => 
+            openFinRuntime.Connect(() => 
+            {
+                //Update the conection status:
+                Utils.InvokeOnUiThreadIfRequired(this, () => { this.lblConnectionStatus.Text = "Connected"; });
+
+                //subscribe to chart-click messages from the chartEmbeddedView
+                openFinRuntime.InterApplicationBus.subscribe(chartEmbeddedView.OpenfinApplication.getUuid(), "chart-click", (senderUuid, topic, data) =>
                 {
-                    this.lblConnectionStatus.Text = "Connected";
-                }));
+                    var dataAsJObject = JObject.FromObject(data);
+                    Utils.InvokeOnUiThreadIfRequired(this, () => { 
+                        label5.Text = string.Format("Key:{0}, {1} at {2}", dataAsJObject.GetValue("key"), dataAsJObject.GetValue("y"), dataAsJObject.GetValue("x")); 
+                    });
+                });
+            });
         }
 
     }
