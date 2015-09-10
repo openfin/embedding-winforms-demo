7273e40f52a368d4dd706ff6cf4510f1754ae665
Ricardo de pena <ricardo.depena@gmail.com>
2015-09-10 17:57:30 -0400

    updated for the latest API version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,10 +45,7 @@ namespace EmbeddingWindowsExample
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label2
@@ -107,45 +104,20 @@ namespace EmbeddingWindowsExample
             this.label5.Size = new System.Drawing.Size(0, 20);
             this.label5.TabIndex = 10;
             // 
-            // tabControl1
+            // panel1
             // 
-            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(13, 100);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1310, 591);
-            this.tabControl1.TabIndex = 11;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1302, 565);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1302, 550);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1335, 595);
+            this.panel1.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1335, 703);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -155,7 +127,6 @@ namespace EmbeddingWindowsExample
             this.MinimumSize = new System.Drawing.Size(560, 560);
             this.Name = "Form1";
             this.Text = "Embeded OpenFin Applications";
-            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,9 +139,7 @@ namespace EmbeddingWindowsExample
         private Label label3;
         private Label label4;
         private Label label5;
-        private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
+        private Panel panel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,8 +8,7 @@ namespace EmbeddingWindowsExample
     public partial class Form1 : Form
     {
         private EmbeddedView chartEmbeddedView;
-        private EmbeddedView docsEmbeddedView;
-        const string version = "alpha";
+        const string version = "beta";
         public Form1()
         {
             InitializeComponent();
@@ -23,24 +22,13 @@ namespace EmbeddingWindowsExample
             };
 
             //Create our two embedded view controlls.
-            this.chartEmbeddedView = new EmbeddedView();
-            this.docsEmbeddedView = new EmbeddedView();
+            chartEmbeddedView = new EmbeddedView();
 
             //Add the embedded view controlls to each tabPage
-            tabPage1.Controls.Add(chartEmbeddedView);
-            tabPage2.Controls.Add(docsEmbeddedView);
+            panel1.Controls.Add(chartEmbeddedView);
 
             //Initialize the chart view by passing the runtime Options and the ApplicationOptions
             chartEmbeddedView.Initialize(runtimeOptions, new ApplicationOptions("of-chart-tab", "of-chart-tab-uuid", "http://cdn.openfin.co/embed-web/chart.html"));
-
-            //We want to re-use the chart application and create a new window for it, lets wait until its ready.
-            chartEmbeddedView.OnReady += (sender, e) =>
-            {
-                var windowOptions = new WindowOptions("jsdocs", "http://cdn.openfin.co/jsdocs/3.0.1.5/");
-                //by passing the runtimeOptions, an existing application and windowOptions we will create a new window for that application that will be embedded in our view.
-                docsEmbeddedView.Initialize(runtimeOptions, chartEmbeddedView.OpenfinApplication, windowOptions);
-            };
-
 
             //We can get the instance of the singleton runtime object by usig the GetRuntimeInstance function and passing 
             var openFinRuntime = Runtime.GetRuntimeInstance(runtimeOptions);
